af469e1006bf65895c68e40ae73ad75c0f9f6ee2
kloik <kloikfriendq@gmail.com>
2016-11-21 20:48:39 +0530

    First hello world

---
+++ app/controllers/index.server.controller.js
@@ -0,0 +1,5 @@
+exports.render = function(req, res){
+	res.send('Hello World!');
+};
+
+
--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-var express = require('express');
+var express = require('./config/express');
 
 var app = express();
 
-
-app.get('/help', function(req, res){
-	res.send('There is no help');
-});
-
-app.use('/', function(req, res){
-	res.redirect('https://goo.gl');
-});
-
-app.listen(3000);
-console.log('Server running on http://localhost:3000');
+app.listen(3031);
 
 module.exports = app;
+
+console.log('Server running at http://localhost:3031/');
